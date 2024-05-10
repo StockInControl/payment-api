0a72888d6b5976866c8b8efa3ea8782b40489baf
Bhavesh588 <sbhavesh588@gmail.com>
2024-05-10 19:25:40 +0530

    Pushing the code

---
+++ routes/paymentRoutes.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+const { v4: uuidv4 } = require("uuid");
+
+// Payment
+router.get("/", function (req, res) {
+    const order_id = uuidv4();
+    const customer_id = req.query.customer_id;
+    const customer_name = req.query.customer_name;
+    const email_id = req.query.email_id;
+    const phone_number = req.query.phone_number;
+    const amount = req.query.amount;
+    const merchant_id = req.query.merchant_id;
+    console.log(order_id, customer_id, customer_name, email_id, phone_number, amount, merchant_id);
+    if (
+        order_id === undefined ||
+        customer_id === undefined ||
+        customer_name === undefined ||
+        email_id === undefined ||
+        phone_number === undefined ||
+        amount === undefined ||
+        merchant_id === undefined
+    ) {
+        res.status(500).send("Some Fields are missing");
+    } else {
+        res.send("Payment");
+    }
+});
+
+module.exports = router;
