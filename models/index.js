4a4458bfd506f0859d0f0ffd6a6dd63419977400
Bhavesh588 <sbhavesh588@gmail.com>
2024-05-15 16:04:30 +0530

    Changing

--- models/index.js
+++ models/index.js
@@ -5,18 +5,14 @@ const path = require("path");
 const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
-const config = require("../config2/config2.json")[env];
+const config = require(__dirname + "/../config2/config2.json")[env];
 const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
     sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-    sequelize = new Sequelize(config.database, config.username, config.password, {
-        host: config.host,
-        dialect: "mysql",
-        dialectModule: require("mysql2"),
-    });
+    sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs.readdirSync(__dirname)
